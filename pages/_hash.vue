d266b8b6a39fc12e71ad06168443b108d6443eeb
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-04-08 13:24:48 +0200

    fix(error): show current hash in error modal

--- pages/_hash.vue
+++ pages/_hash.vue
@@ -52,7 +52,7 @@
           </div>
           <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
             <h3 class="text-lg leading-6 font-medium text-gray-900">
-              Paste /test not found.
+              Paste {{ $route.params.hash }} not found.
             </h3>
             <div class="mt-2">
               <p class="text-sm leading-5 text-gray-500">
